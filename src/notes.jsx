ee65b7a0ae32b8bb5446a3eb7eebdadc2aee489d
VerenaSchwachmeyer <Vronus@gmx.de>
2023-02-23 12:09:53 +0100

    rendering of toDos works now. style css updated as well

--- src/notes.jsx
+++ src/notes.jsx
@@ -1,23 +1,50 @@
 // import Input from './input'
 import NewTask from "./task";
 import { useState } from "react";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Note() {
   const [toDo, setToDo] = useState([]);
 
-  function addToDo(e) {
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   setToDo([...toDo, { task: e.target.firstChild.value }]);
+  //   e.target.firstChild.value = "";
+  // }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setToDo([...toDo, { task: e.target.firstChild.value }]);
-    e.target.firstChild.value = "";
-  }
+    if (!e.target.firstChild.value) return;
+    setToDo((prev) => [...prev, e.target.firstChild.value]);
+  };
 
   return (
     <div className="Black_field">
-      <form className="Form" action="" onSubmit={addToDo}>
+      <form className="Form" action="" onSubmit={handleSubmit}>
         <input type="text" placeholder="Neue Aufgabe" />
         <button tabIndex="2">Hinzufügen</button>
       </form>
-      {/* <NewTask /> */}
+      <ul>
+        {toDo.map((task, index) => (
+          <div className="newTask">
+            <div className="task" key={index}>
+              {task}
+            </div>
+            <span id="delete" className="task" onclick="deleteTask(event)">
+              <FontAwesomeIcon icon={faTrash} />
+            </span>
+            <span id="edit" className="task" onClick="editTask(event)">
+              <FontAwesomeIcon icon={faEdit} />
+            </span>
+            <span id="check" className="task" onClick="checkTask(event)">
+              <FontAwesomeIcon icon={faCheck} />
+            </span>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/task.jsx
+++ src/task.jsx
@@ -1,24 +1,30 @@
 // import { FontAwesomeIcon } from '@fortawesome/free-solid-svg-icons'
-
-function newTask () {
-    return (
-        <div className="New_Task">
-                <input id="description-Aufgabe" className="description" type="submit" value="submit" onsubmit="replaceEditedTask()" disabled />
-                
-                <span id="delete" className="task" onclick ="deleteTask(event)" >
-                {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-circle-check" /> */}
-                
-                </span>
-
-                <span id="edit" className="task" onclick ="editTask(event)">
-                {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-circle-check" /> */}
-                </span>
-
-                <span id="check" className="task" onclick ="checkTask(event)">
-                {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-circle-check" /> */}
-                </span>
-        </div>
-    )
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function NewTask() {
+  return (
+    <div className="New_Task">
+      <input
+        id="task-description"
+        className="description"
+        type="submit"
+        value="e.target.firstChild.value"
+        //onSubmit="replaceEditedTask()"
+        disabled
+      />
+      <span id="delete" className="task" onclick="deleteTask(event)">
+        <FontAwesomeIcon icon={faTrash} />
+      </span>
+      <span id="edit" className="task" onClick="editTask(event)">
+        <FontAwesomeIcon icon={faEdit} />
+      </span>
+      <span id="check" className="task" onClick="checkTask(event)">
+        <FontAwesomeIcon icon={faCheck} />
+      </span>
+    </div>
+  );
 }
 
-export default newTask
+export default NewTask;
